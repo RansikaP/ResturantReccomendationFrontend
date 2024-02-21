e0aa2e963230d9e1beed6dff422abdf8fbbb784f
AkashSivapalan <akashsivapalan@gmail.com>
2024-02-20 19:02:18 -0500

    RegistrationPage

--- app/src/main/java/com/frontend/MainActivity.kt
+++ app/src/main/java/com/frontend/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.frontend
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,38 +11,23 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.tooling.preview.Preview
 import com.frontend.ui.theme.ResturantReccomendationFrontendTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            ResturantReccomendationFrontendTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Michael")
-                }
-            }
+        setContentView(R.layout.home_page)
+
+        val registerButton = findViewById<Button>(R.id.register)
+
+        registerButton.setOnClickListener {
+            // Start another activity
+            val intent = Intent(this, Register::class.java)
+            startActivity(intent)
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ResturantReccomendationFrontendTheme {
-        Greeting("Android")
     }
 }
