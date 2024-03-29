bd4d9a4cbe377d6662b4b844a3d9537676d6ddc5
Ransika <57299043+RansikaP@users.noreply.github.com>
2024-03-29 15:33:50 -0400

    Create Settings.kt

---
+++ app/src/main/java/com/frontend/components/Settings.kt
@@ -0,0 +1,9 @@
+package com.frontend.components
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Settings(padding: PaddingValues) {
+
+}
