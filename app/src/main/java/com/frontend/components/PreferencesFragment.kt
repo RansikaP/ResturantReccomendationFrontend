e97d992328a33e521adac643227a1e9daf004b7f
Ransika <57299043+RansikaP@users.noreply.github.com>
2024-04-16 21:34:57 -0400

    preferences page change from jetpack to xml

--- app/src/main/java/com/frontend/components/PreferencesFragment.kt
+++ app/src/main/java/com/frontend/components/PreferencesFragment.kt
@@ -4,22 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
+import com.frontend.R
 
 class PreferencesFragment : Fragment() {
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        return ComposeView(requireContext()).apply {
-            setContent {
-                Preferences(padding = PaddingValues(start = 20.dp, end = 20.dp))
-            }
-        }
+    ): View? {
+        // Inflate the layout for this fragment
+
+        return inflater.inflate(R.layout.preferences, container, false)
     }
 }
+
